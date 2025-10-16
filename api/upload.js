6c490055d942ae8afa2b64bd2369e189dd03c508
Himaruofficial <himaruofficial1@gmail.com>
2025-10-17 00:10:19 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -27,5 +27,5 @@ export default async function handler(req, res) {
   });
 
   const data = await response.json();
-  res.status(200).json({ url: data?.url ? `https://${data.url}` : null });
+  res.status(200).json({ url: data?.alias ? `https://${data.alias}` : null });
 }
